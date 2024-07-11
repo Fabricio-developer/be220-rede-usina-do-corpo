59aa89d917cda136a41e5263282e695af07fdf40
fabricio <120188885+Fabricio-developer@users.noreply.github.com>
2024-07-11 20:55:39 +0100

    Feat: add basic icon

--- src/app/atoms/icon/icon.component.ts
+++ src/app/atoms/icon/icon.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-icon',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./icon.component.scss'],
 })
 export class IconComponent  implements OnInit {
+
+  @Input() iconIos!: string;
+  @Input() iconMd!: string;
+  @Input() slotIcon!: string;
 
   constructor() { }
 
