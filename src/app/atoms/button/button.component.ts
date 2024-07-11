8a7f3dad7fb66554055a9a2d65ef7bfcf6579170
fabricio <120188885+Fabricio-developer@users.noreply.github.com>
2024-07-11 20:52:39 +0100

    Feat: add first version

--- src/app/atoms/button/button.component.ts
+++ src/app/atoms/button/button.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent  implements OnInit {
-
+export class ButtonComponent implements OnInit {
+  @Input() class!: string;
+  @Input() size: string = 'small';
+  @Input() shape: string = '';
+  @Input() fill: string = '';
+  @Input() color!: string;
+  @Input() icon!: string;
+  @Input() text!: string;
+  @Input() iconIos!: string;
+  @Input() iconMd!: string;
+  @Input() slotIcon!: string;
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
