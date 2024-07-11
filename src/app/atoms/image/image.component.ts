76375b6254591a4525468576a0d05c9ebf46a41e
fabricio <120188885+Fabricio-developer@users.noreply.github.com>
2024-07-11 20:29:19 +0100

    Feat: add image starter

--- src/app/atoms/image/image.component.ts
+++ src/app/atoms/image/image.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.scss'],
 })
-export class ImageComponent  implements OnInit {
+export class ImageComponent implements OnInit {
+  @Input() image!: string;
+  @Input() alt!: string;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log(this.image)
+
+  }
 
 }
