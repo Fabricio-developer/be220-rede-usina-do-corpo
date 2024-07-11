9609c23672c5402eed7399b62b573bf20c47d640
fabricio <120188885+Fabricio-developer@users.noreply.github.com>
2024-07-11 21:07:16 +0100

    Feat: add input

--- src/app/atoms/text/text.component.ts
+++ src/app/atoms/text/text.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-text',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./text.component.scss'],
 })
 export class TextComponent  implements OnInit {
-
+  @Input() text!: string
+  @Input() color!: string
   constructor() { }
 
   ngOnInit() {}
